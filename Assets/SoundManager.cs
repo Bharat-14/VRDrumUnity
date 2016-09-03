b0eb485d22be2c1b7a34ad1f1424bdaee2929733
chetananand <chetananand@smu.edu.sg>
2016-09-04 00:11:13 +0800

    Added sound on drums

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+//	 AudioSource audio1;
+//	AudioSource audio2;
+//	AudioSource audio3;
+//	AudioSource audio4;
+//	public AudioClip clip;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+//		void OnCollisionEnter(Collision collision) {
+//		print ("Test");
+//
+//		if (collision.relativeVelocity.magnitude > 2)
+//			audio.Play();
+//
+//	}
+
+
+	void OnTriggerEnter(Collider other) {
+		print ("Test Trigger" + other);
+		other.GetComponent<AudioSource> ().Play ();
+
+
+
+	}
+}
